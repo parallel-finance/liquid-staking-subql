a799f25002adb870a2002b92938d089b05d0503e
xsteadybcgo <xw4585@gmail.com>
2022-04-02 17:13:05 +0800

    fix: some naming issue

--- src/handlers/metadata.ts
+++ src/handlers/metadata.ts
@@ -1,32 +1,37 @@
+import { SubstrateEvent } from "@subql/types";
 import { Vec, u16, Option } from "@polkadot/types";
 import { BN_ZERO, BN } from "@polkadot/util";
 import { PalletAssetsAssetAccount } from "@polkadot/types/lookup";
-import { Metadata, Staker, Ledger, StakingAction } from "../types";
+import { Metadata, Ledger, StakingAction } from "../types";
 import { createAddress } from "../utils";
-import { Header } from "@polkadot/types/interfaces";
+import { updateStakingAction } from "./stakingAction";
 
 const derivativeIndexList = api.consts.liquidStaking
   .derivativeIndexList as Vec<u16>;
 
-export async function updateMetadataTotalStakers(
-  header: Header,
-  stakerAddress: string
+export async function updateMetadataTotalStakersAndStakingAction(
+  event: SubstrateEvent
 ) {
-  const parentRecord = await Metadata.get(header.parentHash.toString());
-  let metadataRecord = await Metadata.get(header.hash.toString());
+  const blockHash = event.block.block.header.hash.toString();
+  const address = event.extrinsic.extrinsic.signer.toString();
+  const parentRecord = await Metadata.get(
+    event.block.block.header.parentHash.toString()
+  );
+  let metadataRecord = await Metadata.get(blockHash);
   if (!metadataRecord) {
-    metadataRecord = new Metadata(header.hash.toString());
+    metadataRecord = new Metadata(blockHash);
     metadataRecord.totalStakers = parentRecord?.totalStakers
       ? parentRecord.totalStakers
       : 0;
   }
-  if (!(await Staker.get(stakerAddress))) {
+  if ((await StakingAction.getByAddress(address)).length === 0) {
     metadataRecord.totalStakers += 1;
   }
   await metadataRecord.save();
+  await updateStakingAction(event);
 }
 
-export async function updateMetadataTotalStaked(blockHash) {
+export async function updateMetadataTotalLocked(blockHash) {
   const poolAccount = createAddress("par/lqsk");
   const stakingCurrency = api.consts.liquidStaking.stakingCurrency;
   const { balance } = await (
--- src/handlers/staker.ts
+++
@@ -1,8 +0,0 @@
-import { Staker } from "../types";
-
-export async function updateStaker(address) {
-  if (!(await Staker.get(address))) {
-    const record = new Staker(address);
-    await record.save();
-  }
-}
