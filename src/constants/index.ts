b0d3cd83f78ff874c0e32cb80d1372a5fc368232
Cheng JIANG <alex_cj96@foxmail.com>
2022-05-21 17:08:59 +0800

    being compatible with old farming pallet
    Signed-off-by: Cheng JIANG <alex_cj96@foxmail.com>

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,3 +6,6 @@ export const derivativeIndexList = api.consts.liquidStaking
 export const stakingCurrency = api.consts.liquidStaking.stakingCurrency as u32
 
 export const liquidCurrency = api.consts.liquidStaking.liquidCurrency as u32
+
+export const nativeCurrency = api.consts.currencyAdapter
+  .getNativeCurrencyId as u32
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,34 @@
-export { default as createAddress } from './createAddress'
+import { AssetId } from '@parallel-finance/types/interfaces'
+import { encodeAddress } from '@polkadot/keyring'
+import { stringToU8a, bnToU8a, u8aConcat } from '@polkadot/util'
+import { blake2AsU8a, decodeAddress } from '@polkadot/util-crypto'
+
+const EMPTY_U8A_32 = new Uint8Array(32)
+
+export const createAddress = (id: string): string =>
+  encodeAddress(
+    u8aConcat(stringToU8a(`modl${id}`), EMPTY_U8A_32).subarray(0, 32)
+  )
+
+export const loansFarmingRewardAccountId = (): string => {
+  const entropy = blake2AsU8a(
+    u8aConcat(
+      stringToU8a('loans/farming'),
+      decodeAddress(createAddress('par/loan'))
+    ),
+    256
+  )
+  return encodeAddress(entropy)
+}
+
+export const farmingPoolAccountId = (assetId: number): string => {
+  const entropy = blake2AsU8a(
+    u8aConcat(
+      stringToU8a('modlpy/liquidity'),
+      decodeAddress(createAddress('par/farm')),
+      bnToU8a(assetId, 32).reverse()
+    ),
+    256
+  )
+  return encodeAddress(entropy)
+}
